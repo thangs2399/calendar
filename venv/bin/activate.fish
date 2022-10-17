5ca4202f18f97f89fd6f2a0cb87604674db08f83
Solomon <thangs@my.canisius.edu>
2022-10-17 07:53:44 -0400

    modified main.py

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/thangs/Desktop/capp/basic-web/venv"
+set -gx VIRTUAL_ENV "/home/thangs/Desktop/csc395-group-project/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
