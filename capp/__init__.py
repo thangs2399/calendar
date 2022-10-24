91dfb724b8d248591fdd50381816e8b7e4ea19f0
thangs <thangs@my.canisius.edu>
2022-10-23 20:04:25 -0400

    clean up some files

---
+++ capp/__init__.py
@@ -0,0 +1,59 @@
+#################### IMPORTS ####################
+
+import os
+
+from flask import Flask, render_template
+
+
+
+#################### App Factory ####################
+
+def create_app(test_config=None):
+
+    ############### APP CONFIG ###############
+
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'capp.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+
+
+    ############### DATABASE ###############
+
+    from . import db
+    db.init_app(app)  # initialize database
+    with app.app_context():
+        db.init_db()
+
+
+    ############### AUTH ###############
+
+    from . import auth
+    app.register_blueprint(auth.auth)  # register bp
+
+
+    ############### ROUTES/VIEWS ###############
+
+    from . import views
+    app.register_blueprint(views.views)  # register bp
+
+
+    ########################################
+
+    return app # return the app
---
+++ capp/views.py
@@ -0,0 +1,107 @@
+
+############################## IMPORTS ##############################
+
+# 1
+from xmlrpc.client import Boolean
+from flask import Blueprint, request, render_template, session, g, redirect
+import functools
+from capp.db import get_db
+from capp.actions import sessionExists
+
+
+############################## VIEWS ##############################
+
+views = Blueprint("views", __name__)
+
+
+
+############### HOMEPAGE/Calendar ###############
+
+@views.route("/")
+@views.route("/calendar-monthly")
+def homepage():
+    
+    if (not sessionExists()):
+
+        return redirect("/auth/login")
+
+    return render_template('homepage.html')
+
+@views.route("/calendar-weekly")
+def weeklyView():
+    return "weekly view"
+
+@views.route("/calendar-daily")
+def dailyView():
+    return "daily view"
+
+
+
+############### PROFILE ###############
+
+@views.route("/profile")
+def profile():
+
+    if (not sessionExists()):
+
+        return redirect("/auth/login")
+
+    user_id = session.get('user_id')
+
+    db = get_db()
+
+    user =  db.execute(
+            'SELECT username, email FROM users WHERE user_id = ?', (user_id,)
+        ).fetchone()
+
+    return render_template('profile.html', username = user["username"] , email = user["email"])
+
+
+
+############### GROUPS ###############
+
+@views.route("/groups")
+def gorups():
+
+    if (not sessionExists()):
+
+        return redirect("/auth/login")
+
+    return render_template('groups.html')
+
+
+
+############### HELP ###############
+
+@views.route("/help")
+def help():
+
+    if (not sessionExists()):
+
+        return redirect("/auth/login")
+
+    return render_template('help.html')
+
+
+
+############### SETTINGS ###############
+
+@views.route("/settings")
+def settings():
+
+    if (not sessionExists()):
+
+        return redirect("/auth/login")
+
+    return render_template('settings.html')
+
+
+
+
+# PAGE NOT FOUND
+@views.errorhandler(404)
+def not_found(e):
+    return "<h1>PAGE NOT FOUND</h2>"
+
+
+
